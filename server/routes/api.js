da14633e47abf99c3f5cb042f808da9f8e6888ae
Matthew Garza <mattg614@gmail.com>
2023-02-18 18:07:39 -0500

    Middleware placed in rewards route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const rewardsController = require('../controllers/rewardsController');
+
 const router = express.Router();
 
-
-router.get('/', (req, res) => res.json('Test Express Connection'));
+router.post(
+  '/rewards/',
+  rewardsController.sanitizeInputs,
+  rewardsController.calculatePoints,
+  rewardsController.groupCustomers,
+  rewardsController.groupTotals,
+  (req, res) => {
+    return res.status(200).json(res.locals.groupedTransactions);
+  },
+);
 
 module.exports = router;
