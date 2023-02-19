75ae99d02c30c8f76a4cd731a0029e4069d19658
Matthew Garza <mattg614@gmail.com>
2023-02-19 00:23:57 -0500

    Adds fileController middlware

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const rewardsController = require('../controllers/rewardsController');
+const fileController = require('../controllers/fileController');
 
 const router = express.Router();
 
 router.post(
   '/rewards/',
+  fileController.readFile,
   rewardsController.sanitizeInputs,
   rewardsController.calculatePoints,
   rewardsController.groupCustomers,
