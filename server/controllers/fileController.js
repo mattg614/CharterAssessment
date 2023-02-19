39d1c2e53dbbed5401d42d29284a39fc555648d4
Matthew Garza <mattg614@gmail.com>
2023-02-19 15:29:06 -0500

    Adds try catch and error handling to fileController

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -6,9 +6,18 @@ const fs = require('fs');
 const fileController = {};
 
 fileController.readFile = (req, res, next) => {
-  const { fileAddress } = req.body;
-  const fileContent = JSON.parse(fs.readFileSync(fileAddress, 'utf8'));
-  res.locals.fileContent = fileContent;
-  return next();
+  try {
+    const { fileAddress } = req.body;
+    const fileContent = fs.readFileSync(fileAddress, 'utf8');
+    res.locals.fileContent = JSON.parse(fileContent);
+    return next();
+  } catch (error) {
+    return next({
+      log: `Encountered error trying to load file at ${req.body.fileAddress}`,
+      status: 400,
+      message: {err: `Error ocurred trying to load file at ${req.boyd.fileAdress}`}
+    })
+  }
+
 };
 module.exports = fileController;
