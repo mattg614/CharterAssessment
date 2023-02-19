3707e127cf093c61b151e2829d3c8a190eced7ac
Matthew Garza <mattg614@gmail.com>
2023-02-19 11:30:38 -0500

    Adds dropdown menu to select desired customer

---
+++ client/components/customerSelector.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+const CustomerSelector = ({ handleSubmit, customerIds }) => {
+  const options = [];
+  customerIds.forEach(id => {
+    options.push(<option value={id} key={id}>Customer Id: {id}</option>)
+  });
+  
+  return (
+    <form onSubmit = {handleSubmit} >
+      <label>Select the desired Customer Id
+      <select>
+        {options}
+      </select>
+      </label>
+      <input type='submit' value='Submit' />
+    </form>
+  )
+}
+export default CustomerSelector;
