b4d37e9deae8908402b3475cdbbfbae9337e5b32
Matthew Garza <mattg614@gmail.com>
2023-02-19 11:31:04 -0500

    Pulls destructuring of props into params definition

--- client/components/fileLoader.jsx
+++ client/components/fileLoader.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-const FileLoader = props => {
-  const { handleSubmit } = props;
-
+const FileLoader = ({ handleSubmit }) => {
   return (
     <div className='file-loader-card'>
       <form onSubmit={handleSubmit}>
